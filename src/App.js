faa7d492e7482c5c05a58e230ef1cedbbe5fa2ed
dipikaash <ash.dipika2012@gmail.com>
2023-07-31 15:56:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="header">
+      
+      <div className="logo">
+          <img alt="Happiest Minds" className="classLogo" src="https://www.bing.com/th?id=OIP.Cxwp2AvIVAXsGCHrmLX_VwHaC5&w=350&h=137&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" />
+          Resource Management Portal
+      </div>
+  <div className="nav-items">
+      <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+      </ul>
+  </div>
+</div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +30,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
